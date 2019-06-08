21f4fb435298ea94b42b5f378b8530406caafaa9
bilalragimli <49587796+bilalragimli@users.noreply.github.com>
2019-06-08 17:00:10 +0400

    Add files via upload

---
+++ ad/ad/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    Console.WriteLine("Zehmet olmasa adinizi daxil edin");
+            //    string a = Console.ReadLine();
+            //    Console.WriteLine("Zehmet olmasa yashinizi daxil edin");
+            //    string b = Console.ReadLine();
+            //    if(a=="Admin" && b == "12345")
+            //    {
+            //        Console.WriteLine("Xosh gelmisen Admin");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Bele bir admin yoxdur");
+            //    }
+            //}
+
+            Student telebe1 = new Student();
+            telebe1.name = "Bilal";
+
+            Student telebe2 = new Student();
+            telebe2.name = "Cavid";
+
+            telebe1.setPassword("bilal123");
+            Console.WriteLine(telebe1.getPassword(Console.ReadLine()));
+        }
+    }
+}
---
+++ ad/ad/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ad
+{
+    class Student
+    {
+        public string name;
+        private string password;
+
+        public void setPassword(string a)
+        {
+            password = a;
+        }
+   
+        public bool getPassword(string b)
+        {
+            if (password == b)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+    
+}
